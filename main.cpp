c0babf6ec99fcfc246b24a6379be1ac420e7e483
martinrybka2000 <rybatinek14@gmail.com>
2022-04-07 21:24:05 +0200

    Done some test on Nodes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 
 #include "node.hpp"
-// #include "operations.hpp"
-// #include "functions.hpp"
+#include "operations.hpp"
+#include "functions.hpp"
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
-    std::cout << "Test 2\n";
 
-    std::shared_ptr<ConstantNode> pi(new ConstantNode(3.14159265358979323846));
-    std::shared_ptr<ConstantNode> e(new ConstantNode(2.71828182845904523536));
+    std::vector<Scalar> values;
 
-    std::cout << pi->Type() << pi->Value();
+    values.push_back(std::make_shared<ConstantNode>(2.13 / 2.0));
+    values.push_back(std::make_shared<Node>());
+    values.push_back(std::make_shared<VariableNode>(1));
+    std::shared_ptr<AdditionNode> adddd(new AdditionNode({values[2], values[0]}));
+    std::shared_ptr<SinNode> sinnnn(new SinNode({adddd}));
+
+    for (auto val : values)
+    {
+        std::cout << "Type: " << val->Type() << "Value: " << val->Value() << std::endl;
+    }
+
+    std::cout << " = " << adddd->Value() << "\n";
+    std::cout << "sin = " << sinnnn->Value() << "\n";
 }
